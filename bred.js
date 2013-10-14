834c23865d5e9e8751501081f6e6a26b713c8de7
Vladimir Shestakov <boolive@yandex.ru>
2013-10-15 01:51:50 +0600

    Версия 2.0. Генератор по всей html разметке.

--- bred.js
+++ bred.js
@@ -1,53 +1,310 @@
 /**
  * Бредогенератор
  *
- * @version 1.0
- * @date 10.10.2013
+ * @version 2.0
+ * @date 14.10.2013
  * @author Vladimir Shestakov <boolive@yandex.ru>
- * @uss JQuery, chain.js
+ * @use JQuery, chain.js
  * @constructor
  */
-var Bred = (function($, Chain){
+var Bred = (function($, Chain, undefined){
+    /**
+     * Строковое представление объекта для его идентификации
+     * @param object
+     * @returns {string}
+     */
+    var hash = function(object){
+        if ($.isPlainObject(object)){
+            var s = '';
+            $.each(object, function(key){
+                if (s.length) s += ',';
+                s += key + ':' + hash(this);
+            });
+            return '{' + s + '}';
+        }
+        return ''+object;
+    };
+    var escapeHTML = (function () {
+        'use strict';
+        var chr = { '"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;' };
+        return function (text) {
+            return text.replace(/[\"&<>]/g, function (a) { return chr[a]; });
+        };
+    }());
+    /**
+     * Узел бреда
+     * @param key
+     * @param info
+     * @param parent
+     * @param prev
+     * @constructor
+     */
+    var BredNode = function(key, info, parent, prev){
+        /** Информация об узле: имя, атрибуты, значение...*/
+        this.info = info || {};
+        this.key = hash(info);
+        /** Следующие и подчиенные узлы. */
+        this.next = [];
+        this.children = [];
+        /** Ссылки на родителький и предыдущий узел*/
+        this.parent = parent;
+        this.prev = prev;
+        /** Индекс подчиненных узлов для быстрого поиска существующего */
+        this.index = {};
+    };
+    /**
+     * Добавление следующего узла
+     * Если уже имеется узел с указанной информацией, то добавляется ссылка на него, иначе создаётся новый
+     * @param info
+     * @returns {*}
+     */
+    BredNode.prototype.addNext = function(info){
+        if (this.parent){
+            var key = this.getKey(info);
+            if (typeof this.parent.index[key] == 'undefined') this.parent.index[key] = new BredNode(key, info, this.parent, this);
+            this.next.push(this.parent.index[key]);
+            return this.parent.index[key];
+        }
+        return this;
+    };
+    /**
+     * Добавление подчиенного узла
+     * Если уже имеется узел с указанной информацией, то добавляется ссылка на него, иначе создаётся новый
+     * @param info
+     * @returns {*}
+     */
+    BredNode.prototype.addChild = function(info){
+        var key = this.getKey(info,0);
+        if (typeof this.index[key] == 'undefined') this.index[key] = new BredNode(key, info, this);
+        this.children.push(this.index[key]);
+        return this.index[key];
+    };
+    /**
+     * Генерация ключа для узла изсходя из его информации и его предыдущих узлов.
+     * @param info
+     * @param size Кол-во предыдущих узлов (префикс)
+     * @returns {*}
+     */
+    BredNode.prototype.getKey = function(info, size){
+        var key = hash(info);
+        var prev = this;
+        if (typeof size === 'undefined') size = 1;
+        while (prev && size-->0){
+            if (!prev._key) prev._key = hash(prev.info);
+            key+="#"+prev._key;
+            prev = prev.prev;
+        }
+        return key;
+    };
+
+    BredNode.prototype.getChild = function(){
+        return this.children[Math.round(Math.random()*(this.children.length-1))];
+    };
+
+    BredNode.prototype.getNext = function(){
+        return this.next[Math.round(Math.random()*(this.next.length-1))];
+    };
+
+    /**
+     * Является ли узел символом пунктуации?
+     * @returns {boolean}
+     */
+    BredNode.prototype.isPunctuation = function(){
+        return this.info.val && /^[.,:!?]+$/.test(this.info.val);
+    };
+
+    BredNode.prototype.render = function(max, callbacks, statistic){
+        // Тег
+        var result = '', contain = '';
+        var tag = typeof this.info.name === 'undefined' || this.info.name==='text' ? false : this.info.name;
+        var is_single = tag && /^(img|br|hr)$/i.test(tag);
+        if (tag) result += '<' + this.info.name;
+        if ($.isPlainObject(this.info.attr)){
+            $.each(this.info.attr, function(key, val){
+                result += ' '+key+'="'+val+'"'
+            });
+        }
+        if (tag) result += is_single ? '/>' : '>';
+        // Значение или подчиенные
+        if (this.children.length > 0){
+//            var cnt = 4;
+//            while (--cnt>0){
+            var max_child = statistic.getCountsSum(this.info.name)+5;
+               contain = this.getChild().render(max_child, callbacks, statistic);
+//            }
+        }else
+        if (this.info.val){
+            if (!this.isPunctuation()) result += ' ';
+            contain = escapeHTML(this.info.val);
+        }
+        if ($.isPlainObject(callbacks) && $.isFunction(callbacks[this.key])){
+            if (!callbacks[this.key].call(this, contain)){
+                contain = '';
+            }
+        }
+        result += contain;
+        //Закрываем
+        if (tag && !is_single) result += '</' + this.info.name + '> ';
+        //Следующий
+        if (max>1 && this.next.length > 0){
+            result += this.getNext().render(max-1, callbacks, statistic);
+        }
+
+        return result;
+    };
+
     /**
      * Генератор бреда
-     * @param src Источник бреда - текс или html
      * @param config Конфигурация бреда - размеры предложений, абзацев, комментов, качества бреда
      * @constructor
      */
-    var Bred = function(src, config){
+    var Bred = function(config){
         this.config = config;
-        this.articles = new Chain();
-        this.comments = new Chain();
-        this.users = [];
-        this.articles.pfx_length = config.quality;
-        this.comments.pfx_length = config.quality;
-        // Создание цепочек из src
-        var self = this;
-        if (/<(\w+)((?:\s+\w+(?:\s*=\s*(?:(?:"[^"]*")|(?:'[^']*')|[^>\s]+))?)*)\s*(\/?)>/.test(src)){
-            var $articles = $('<div>').append(src).find('article');
-            // HTML исходник
-            $articles.each(function(article_i, article){
-                // Вырезаем и обрабатываем комментарии
-                var $com = $(article).find('.comments').remove();
-                $com.find('.username').each(function(user_i, user){
-                    self.users.push($(user).text());
+        // Частотный анализ
+        this.statistic = {
+            group: {},
+            frequency: null,
+            // Добавление сведений о количестве использований элементов в некой группе
+            add: function(group, counted_items){
+                if (typeof this.group[group] === 'undefined') this.group[group] = [];
+                this.group[group].push(counted_items);
+            },
+            // Обработка данных для последующей выдачи случайных величин с учётом частот
+            init: function(){
+                var self = this;
+                this.frequency = {};
+                $.each(this.group, function(key, value){
+                    self.frequency[key] = {};
+                    var cnt = 0;
+                    $.each(value, function(key2, value2){
+                        cnt++;
+                        $.each(value2, function(key3, value3){
+                            if (typeof self.frequency[key][key3] === 'undefined') self.frequency[key][key3] = {/*0:0*/};
+                            if (typeof self.frequency[key][key3][value3] === 'undefined') self.frequency[key][key3][value3] = 0;
+                            self.frequency[key][key3][value3]++;
+                            //self.frequency[key][key3][0]--;
+                        });
+                    });
+
+                    $.each(self.frequency[key], function(key2){
+                        var m = [], v = 0;
+                        //self.frequency[key][key2][0]+=cnt;
+                        $.each(self.frequency[key][key2], function(key3, value3){
+                            m.push({i:value3 + v, cnt: key3});
+                            v+=value3;
+                        });
+                        self.frequency[key][key2] = m;
+                    });
                 });
-                $com.find('.message').each(function(message_i, message){
-                    self.comments = self.comments.addElement(message);
+            },
+            // Случайное количество для элемента в группе с учётом частот
+            randomCount: function(group, item){
+                if (!this.frequency) this.init();
+                if (typeof this.frequency[group][item] !== 'undefined'){
+                    var need =  this.frequency[group][item][this.frequency[group][item].length-1].i;
+                    need = Math.round(Math.random()*need);
+                    var index = 0, high = this.frequency[group][item].length;
+                    while (index < high) {
+                        var mid = (index + high) >>> 1;
+                        this.frequency[group][item][mid].i < need ? index = mid + 1 : high = mid;
+                    }
+                    return this.frequency[group][item][index].cnt;
+                }
+                return 0;
+            },
+            getCounts: function(group){
+                if (typeof this.group[group] !== 'undefined'){
+                    return this.group[group][Math.round(Math.random()*(this.group[group].length-1))];
+                }
+                return {};
+            },
+            getCountsSum: function(group){
+                var sum = 0;
+                $.each(this.getCounts(group), function(){
+                    sum+=this;
                 });
-                // Обработка текста статьи
-                self.articles = self.articles.addElement(article);
-            });
-        }else{
-            // Обработка как текста
-            var lines = src.split("\n");
-            var cnt = lines.length;
-            for (var i=0; i<cnt; i++){
-                this.articles = this.articles.addText(lines[i]);
+                return sum;
             }
+        };
+        this.node = new BredNode();
+    };
+
+    Bred.prototype.add = function(data){
+        this.parse(this.node, data, false, false);
+    };
+
+    Bred.prototype.parse = function(c, element, all, make_p){
+        var self = this,
+            group = element.localName,
+            p = document.createElement('p'); // Для восоздания абзацев
+
+        // Особенности для статьи (отдельная обработка комментов)
+        //if (group == 'article') this.parse($(element).find('.comments').remove()[0], false, false);
+
+        // Особенности для коментария (обрабатываем только его сообщение)
+//            if ($(element).hasClass('comment')){
+//                this.users.push($(element).find('.username'));
+//                element = $(element).find('.message')[0];
+//                make_p = true;
+//                group = 'comment';
+//            }
+
+        // Для статистики, что в текущем элементе
+        //if (typeof this.chains[group] === 'undefined') this.chains[group] = [];
+
+        // Обработка подчиенных узлов (тегов и текстовых)
+        var children = all ? $(element).contents() : $(element).children();
+        var list = [];
+        var counts = {};
+
+        var sub = undefined;
+        children.each(function(i, child){
+            var tag = Node.ELEMENT_NODE === child.nodeType ? child.localName : 'text';
+            // Inline тэги в абзац
+            if (make_p && /^(text|i|u|a|b|i|u|strong|em|code|span|font)$/.test(tag)){
+                p.appendChild(this);
+            }else{
+                if (make_p && p.childNodes.length>0){
+                    sub = sub ? sub.addNext({name:'p'}) : c.addChild({name:'p'});
+                    self.parse(sub, p, true, false);
+                    p = document.createElement('p');
+                    if (typeof counts['p'] === 'undefined') counts['p'] = 0;
+                    counts['p']++;
+                }
+                if (/^(p|h1|h2|h3|h4|h5|h6|ul|ol|li|pre|blockquote|div|article|section|i|u|a|b|i|u|strong|em|code|span|table|tbody|thead|tr|td|sub|font)$/.test(tag)){
+                    var info = {name: tag, attr: {}};
+                    $.each(this.attributes, function() {
+                        if(this.specified) {
+                         info.attr[this.name] = this.value;
+                        }
+                    });
+                    if ($.isEmptyObject(info.attr)) delete info.attr;
+                    sub = sub ? sub.addNext(info) : c.addChild(info);
+                    self.parse(sub, this, true, tag == 'article');
+                    if (typeof counts[tag] === 'undefined') counts[tag] = 0;
+                    counts[tag]++;
+                }else
+                // Разбиваем на текст на слова
+                if (!make_p && tag == 'text'){
+                    var words = child.nodeValue.match(/([A-Za-zА-Яа-яёЁ0-9]+|[.,:!?]+)/g);
+                    if ($.isArray(words)){
+                        $.each(words, function(i,v){
+                            sub = sub ? sub.addNext({name:'text', val:v}) : c.addChild({name:'text', val:v});
+                            if (typeof counts[tag] === 'undefined') counts[tag] = 0;
+                            counts[tag]++;
+                        });
+                    }
+                }
+            }
+        });
+        if (!$.isEmptyObject(counts)){
+            this.statistic.add(group, counts);
         }
-        this.articles = this.articles.root;
-        this.comments = this.comments.root;
+        return list;
+    };
+
+    Bred.prototype.render = function(max, callbacks){
+        return this.node.render(max, callbacks, this.statistic);
     };
 
     /**
@@ -55,33 +312,33 @@ var Bred = (function($, Chain){
      * @param {object} tpl Шаблон статьи.
      * @returns {$}
      */
-    Bred.prototype.render = function(tpl){
-        var $article = tpl.article.clone().show();
-        // Статья
-        $article.find(tpl.places.title).html(this.articles.root.getSentence(3, 8));
-        $article.find(tpl.places.date).text('сегодня в 11:22');
-        $article.find(tpl.places.text).html(this.articles.root.getText(this.config));
-        // Комменты
-        var com = this.comments.isEmpty() ? this.articles.root : this.comments.root;
-        var comment_list = [$article.find(tpl.places.comments)],
-            $comment,
-            i,cnt = Math.round(Math.random()*(this.config.comments.max-this.config.comments.min))+this.config.comments.min;
-        $article.find(tpl.places.comments_cnt).text(cnt);
-        var comment_cfg = {
-            words:{max: 1, min: 15},
-            paragraphs:{max: 1, min: 2},
-            sentences:{max: 1,min: 3}
-        };
-        for (i=0; i<cnt; i++){
-            $comment = tpl.comment.clone();
-            $comment.find(tpl.places.comment_places.username).text((this.users.length > 0)? this.users[Math.round(Math.random() * (this.users.length-1))] : 'Гость');
-            $comment.find(tpl.places.comment_places.date).text('сегодня');
-            $comment.find(tpl.places.comment_places.text).html(com.getText(comment_cfg));
-            comment_list[Math.round(Math.random() * (comment_list.length-1))].append($comment);
-            comment_list.push($comment.find(tpl.places.comment_places.sub));
-        }
-        return $article;
-    };
+//    Bred.prototype.render = function(tpl){
+//        var $article = tpl.$article.clone().show();
+//        // Статья
+//        $article.find(tpl.places.title).html(this.articles.root.getSentence(3, 8));
+//        $article.find(tpl.places.date).text('сегодня в 11:22');
+//        $article.find(tpl.places.text).html(this.articles.root.getText(this.config));
+//        // Комменты
+//        var com = this.comments.isEmpty() ? this.articles.root : this.comments.root;
+//        var comment_list = [$article.find(tpl.places.comments)],
+//            $comment,
+//            i,cnt = Math.round(Math.random()*(this.config.comments.max-this.config.comments.min))+this.config.comments.min;
+//        $article.find(tpl.places.comments_cnt).text(cnt);
+//        var comment_cfg = {
+//            words:{max: 1, min: 15},
+//            paragraphs:{max: 1, min: 2},
+//            sentences:{max: 1,min: 3}
+//        };
+//        for (i=0; i<cnt; i++){
+//            $comment = tpl.$comment.clone();
+//            $comment.find(tpl.places.comment.username).text((this.users.length > 0)? this.users[Math.round(Math.random() * (this.users.length-1))] : 'Гость');
+//            $comment.find(tpl.places.comment.date).text('сегодня');
+//            $comment.find(tpl.places.comment.text).html(com.getText(comment_cfg));
+//            comment_list[Math.round(Math.random() * (comment_list.length-1))].append($comment);
+//            comment_list.push($comment.find(tpl.places.comment.sub));
+//        }
+//        return $article;
+//    };
 
     return Bred;
 
--- main.js
+++ main.js
@@ -14,25 +14,7 @@
         // Генерируем и показываем бред
         $form.on('submit', function(e){
 
-            // Шаблон статьи и комментария
-            var tpl = {
-                comment: $('#bred_comment').remove(),
-                article: $('#bred_article').remove(),
-                places:{
-                    title: '.post_title',
-                    date: '.post_date',
-                    text: '.post_text',
-                    comments: '.post_comments',
-                    comments_cnt: '.post_comments_count',
-                    comment_places: {
-                        username: '.username',
-                        date: '.date',
-                        text: '.message',
-                        sub: '.reply_comments'
-                    }
-                }
-            };
-            var config = {
+            var bred = new Bred({
                 quality: parseInt($form.find('[name="quality"]').val()),
                 words:{
                     max: parseInt($form.find('[name="words[min]"]').val()),
@@ -50,16 +32,65 @@
                     max: parseInt($form.find('[name="comments[min]"]').val()),
                     min: parseInt($form.find('[name="comments[max]"]').val())
                 }
-            };
+            });
+
+            var src = $form.find('[name="src"]').val();
 
             $form.addClass('hide');
             $wait.show();
 
-            $.get($form.find('[name="src"]').val(), function(src){
-                $content.append((new Bred(src, config)).render(tpl));
+            $.get(src, function(data){
+                // Текстовый файл дополняем минимальной html разметкой, так как бредогенератор работает с ней
+                if (/\.txt$/.test(src)){
+
+                }
+                // Удаление лишних <br> и пробельных символов (чтобы небыло "пустых" текстовых узлов в DOM)
+                data = data.replace(/(>)\s+/g,'$1').replace(/\s+(<)/g,'$1').replace(/(<br\/?>\s*){2,}/g,'');
+                data = $('<div>').append(data);
+
+                // Статьи из портянку обрабатываем поштучно, иначе бредогенератор выдаст новую портянку =)
+                if (src == 'habr.html'){
+                    var $div = $('<div>');
+                    data.find('.container > article').each(function(){
+                        bred.add($div.append(this)[0]);
+                        $div.empty();
+                    });
+                }else{
+                    bred.add(data[0]);
+                }
+
+                // Шаблоны статьи и комментария и области для вставки комментов
+                var $comment = $('#bred_comment').remove().show();
+                var $article = $('#bred_article').remove().show();
+                var comments = [$article.find('.post_comments')];
+
+                // Генерируем. Функциями обратного вызова забираем желаемые куски статьи и вставляем в шаблон хабра
+                bred.render(0,{
+                    '{name:article}': function(data){
+                        $article.find('.post_title:first').html('Заголовок поста');
+                        $article.find('.post_date:first').html('сегодня');
+                        $article.find('.post_text:first').html(data);
+                        $article.find('.post_comments_count:first').text(comments.length-1);
+                    },
+                    '{name:div,attr:{class:comment}}': function(data){
+                        var c = $comment.clone();
+                        c.find('time.date:first').text('15 октября 2013 в 09:24');
+                        comments[Math.round(Math.random()*(comments.length-1))].append(c);
+                        comments.push(c.find('.reply_comments:first'));
+                    },
+                    '{name:div,attr:{class:username}}': function(data){
+                        $comment.find('.username:first').text(data);
+                    },
+                    '{name:div,attr:{class:message}}': function(data){
+                        $comment.find('.message:first').html(data);
+                    }
+                });
+                $content.append($article);
+                //bred.statistic.init();
+                console.log(bred);
+                console.log(bred.statistic.getCounts('article'));
                 $wait.hide();
             });
-
             return false;
         })
     });
